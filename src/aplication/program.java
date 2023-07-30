322bf070319fb523633c35f9f993b7f2ad2721cf
CristianoBasto <cristhianobastos@gmail.com>
2023-07-30 13:34:49 -0300

    criado classes do programa

---
+++ src/aplication/program.java
@@ -0,0 +1,19 @@
+package aplication;
+
+import java.util.Scanner;
+
+public class program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
